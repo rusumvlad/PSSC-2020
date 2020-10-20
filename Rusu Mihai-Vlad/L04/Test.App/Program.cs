6ed441efa50aa0e0d0a69b669e1d79ceb1b378aa
RusuVlad99 <vlad.rusu@student.upt.ro>
2020-10-20 19:25:43 +0300

    Rezolvare tema4

--- Rusu Mihai-Vlad/L04/Test.App/Program.cs
+++ Rusu Mihai-Vlad/L04/Test.App/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Profile.Domain.CreateProfileWorkflow;
+using Question.Domain.CreateQuestionWorkflow;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using static Profile.Domain.CreateProfileWorkflow.CreateProfileResult;
+using static Question.Domain.CreateQuestionWorkflow.CreateQuestionResult;
+
+
 
 namespace Test.App
 {
@@ -10,18 +13,26 @@ namespace Test.App
     {
         static void Main(string[] args)
         {
-            var cmd = new CreateProfileCmd("Ion", string.Empty, "Ionescu", "ion.inonescu@company.com");
-            var result = CreateProfile(cmd);
+            //Profile
+            var cmdProfile = new CreateProfileCmd("Ion", string.Empty, "Ionescu", "ion.inonescu@company.com");
+            var resultProfile = CreateProfile(cmdProfile);
+            //Question
+            var cmdQuestion = new CreateQuestionCmd("How to convert int to string in C#?", "I try so hard to convert int to string but i can't, the language that I use is C#.", "C#");
+            var resultQuestion = CreateQuestion(cmdQuestion);
 
-            result.Match(
+            resultProfile.Match(
                     ProcessProfileCreated,
                     ProcessProfileNotCreated,
                     ProcessInvalidProfile
                 );
-
+            resultQuestion.Match(
+                ProcessQuestionCraeted,
+                ProcessQuestionNotCreated,
+                ProcessInvalidQuestion
+                );
             Console.ReadLine();
         }
-
+        //Profile invalid
         private static ICreateProfileResult ProcessInvalidProfile(ProfileValidationFailed validationErrors)
         {
             Console.WriteLine("Profile validation failed: ");
@@ -32,18 +43,47 @@ namespace Test.App
             return validationErrors;
         }
 
+        //Question invalid
+        private static ICreateQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+
+        //Profile not Created
         private static ICreateProfileResult ProcessProfileNotCreated(ProfileNotCreated profileNotCreatedResult)
         {
             Console.WriteLine($"Profile not created: {profileNotCreatedResult.Reason}");
             return profileNotCreatedResult;
         }
 
+        //Question not Created
+        private static ICreateQuestionResult ProcessQuestionNotCreated(QuestionNotCreated questionNotCreatedResult)
+        {
+            Console.WriteLine($"Question not created: {questionNotCreatedResult.Reason}");
+            return questionNotCreatedResult;
+        }
+
+        //Profile Created
         private static ICreateProfileResult ProcessProfileCreated(ProfileCreated profile)
         {
             Console.WriteLine($"Profile {profile.ProfileId}");
             return profile;
         }
 
+        //Question Created
+        private static ICreateQuestionResult ProcessQuestionCraeted(QuestionCreated question)
+        {
+            Console.WriteLine($"Question {question.QuestionId}");
+            Console.WriteLine($"Title: {question.Title}");
+            return question;
+        }
+
+        //Create Profile
         public static ICreateProfileResult CreateProfile(CreateProfileCmd createProfileCommand)
         {
             if (string.IsNullOrWhiteSpace(createProfileCommand.EmailAddress))
@@ -58,10 +98,40 @@ namespace Test.App
             }
 
             var profileId = Guid.NewGuid();
-            var result = new ProfileCreated(profileId, createProfileCommand.EmailAddress);
+            var resultsProfile = new ProfileCreated(profileId, createProfileCommand.EmailAddress);
 
             //execute logic
-            return result;
+            return resultsProfile;
+        }
+
+        //Create Question
+        public static ICreateQuestionResult CreateQuestion(CreateQuestionCmd createQuestionCommand)
+        {
+            if (string.IsNullOrWhiteSpace(createQuestionCommand.Title))
+            {
+                var errors = new List<string>() { "Invalid title" };
+                return new QuestionValidationFailed(errors);
+            }
+            else if (string.IsNullOrWhiteSpace(createQuestionCommand.Body))
+            {
+                var errors = new List<string>() { "Invalid body" };
+                return new QuestionValidationFailed(errors);
+            }
+            else if (string.IsNullOrWhiteSpace(createQuestionCommand.Tags))
+            {
+                var errors = new List<string>() { "Invalid tag" };
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (new Random().Next(10) > 1)
+            {
+                return new QuestionNotCreated("Question could not be verified");
+            }
+
+            var questionId = Guid.NewGuid();
+            var resultsQuestion = new QuestionCreated(questionId, createQuestionCommand.Title);
+
+            return resultsQuestion;
         }
     }
 }
