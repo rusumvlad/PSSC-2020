1e6855fc6f364187fabc3a41ab4b635c0f2d0e8b
RusuVlad99 <vlad.rusu@student.upt.ro>
2020-10-26 17:35:54 +0200

    Start L05

---
+++ Rusu Mihai-Vlad/L05/Question.Domain/CreateQuestionWorkflow/CreateQuestionCmd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    public struct CreateQuestionCmd
+    {
+        [Required]
+        public string Title { get; private set; }
+        [Required]
+        public string Body { get; private set; }
+        [Required]
+        public string Tags { get; private set; }
+
+        public CreateQuestionCmd(string title, string body, string tags)
+        {
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+    }
+}
