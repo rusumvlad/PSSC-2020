a2a7ba7d44aff2cac2b2af78aaeedd11248e966c
RusuVlad99 <vlad.rusu@student.upt.ro>
2020-11-11 18:31:23 +0200

    Rezolvare tema6

---
+++ Rusu Mihai-Vlad/L06/Tema6/Inputs/SendAckToQuestionOwnerCmd.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tema6.Inputs
+{
+    public class SendAckToQuestionOwnerCmd
+    {
+        [Required]
+        public int ReplyId { get; }
+
+        [Required]
+        public int QuestionId { get; }
+
+        [Required]
+        public string Answer { get; }
+        public string Text { get; internal set; }
+
+        public SendAckToQuestionOwnerCmd(int replyId, int questionId, string answer)
+        {
+            ReplyId = replyId;
+            QuestionId = questionId;
+            Answer = answer;
+        }
+    }
+}
---
+++ Rusu Mihai-Vlad/L06/Tema6/Inputs/SendAckToReplyAuthorCmd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tema6.Inputs
+{
+    public class SendAckToReplyAuthorCmd
+    {
+        [Required]
+        public int ReplyId { get; }
+
+        [Required]
+        public int QuestionId { get; }
+
+        [Required]
+        public string Answer { get; }
+
+        public SendAckToReplyAuthorCmd(int replyId, int questionId, string answer)
+        {
+            ReplyId = replyId;
+            QuestionId = questionId;
+            Answer = answer;
+        }
+    }
+}
